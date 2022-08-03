2ef0e11f40d099a2f9ee20354755f7e3f5d8cd4d
milessteamaccount <ronaldlamdev@gmail.com>
2022-08-02 19:24:19 -0500

    added nunito font from google fonts and created a logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0A192F] text-gray-300'>
-      <div></div>
+      <div>
+        <span className='text-5xl text-pink-600'>RL</span>
+      </div>
     </div>
   )
 }
