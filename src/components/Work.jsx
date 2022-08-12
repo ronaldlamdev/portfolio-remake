b522510df556af20977d7cfadf854b960c6529b5
milessteamaccount <ronaldlamdev@gmail.com>
2022-08-11 19:58:03 -0500

    created hover overlay to show tech on image divs in work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -15,11 +15,17 @@ const Work = () => {
 
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
           {projects.map((project, index) => (
-            <div className='flex flex-col justify-center items-center' key={index}>
-              <div>
-              <img src={project.thumbnail}/>
+            <div className='flex flex-col justify-center items-center w-full' key={index}>
+              <div className='max-w-[400px] w-full relative flex justify-center group'>
+                <img className='w-full rounded-md group-hover:opacity-30 duration-300' src={project.thumbnail}/>
+                <div className='absolute p-4 text-white top-[25%] flex justify-center items-center text-center opacity-0 group-hover:opacity-100 duration-300'>
+                  <p className='text-2xl'>{project.tech}</p>
+                </div>
               </div>
-              <h2>{project.title}</h2>
+              <div className='p-6 relative w-full text-center'>
+                <h2 className='text-2xl'>{project.title}</h2>
+                <div></div>
+              </div>
             </div>
           ))}
         </div>
