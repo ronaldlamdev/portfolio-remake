ab258d395e638d767cc5e225478a9ea04643e753
milessteamaccount <ronaldlamdev@gmail.com>
2022-08-13 00:09:26 -0500

    added resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi';
 import {BsFillPersonLinesFill} from 'react-icons/bs';
 import {Link} from 'react-scroll';
+import Resume from '../assets/WebDevUpdatedResume.pdf';
 
 const Navbar = () => {
 
@@ -107,7 +108,7 @@ const Navbar = () => {
             </Link>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-            <a className='flex justify-between items-center w-full text-white' href='/'>
+            <a className='flex justify-between items-center w-full text-white' href={Resume} target="_blank">
               Resume <BsFillPersonLinesFill size={30}/>
             </a>
           </li>
